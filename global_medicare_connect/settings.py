2e751f858aec6ae6cb5c6685cd5d981d1cf8b899
ramisalamya1 <ramisalamya0@gmail.com>
2025-04-08 21:35:17 +0600

    Added Hospital Search Feature

--- global_medicare_connect/settings.py
+++ global_medicare_connect/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'booking',
-    'hospital_search',
     'admin_management',
 ]
 
--- hospital_search/admin.py
+++ hospital_search/admin.py
@@ -1,10 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-
-from .models import Hospital, Doctor, Facility, Review
-
-admin.site.register(Hospital)
-admin.site.register(Doctor)
-admin.site.register(Facility)
-admin.site.register(Review)
