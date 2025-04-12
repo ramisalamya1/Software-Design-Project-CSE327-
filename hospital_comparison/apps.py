ac719746cae3670767b4ec132738c4bfed006db9
TaniaHussain430 <taniahussain430@gmail.com>
2025-04-12 12:26:06 +0600

    fixed hospital_comparison name to avoid clash

--- hospital_comparison/apps.py
+++ hospital_comparison/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class HospitalSearchConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'hospital_search'
+    name = 'hospital_comparison'
