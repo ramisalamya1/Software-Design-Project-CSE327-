ec1a97f7196f9ab5d74adf0ba7fa921e99c72f9a
sayed <sayedashraf.opin@gmail.com>
2025-04-18 15:24:20 +0600

    editing the media files

--- global_medicare_connect/urls.py
+++ global_medicare_connect/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from booking.views import *
@@ -29,3 +30,6 @@ urlpatterns = [
     path('register/', views.register, name='register'), 
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
