a33dc4a7b7b2eed7b1eeea9e3eacdbde75c1d5aa
sayed <sayedashraf.opin@gmail.com>
2025-04-19 20:25:30 +0600

    fixing the bug of views.py

--- medical_records/views.py
+++ medical_records/views.py
@@ -8,7 +8,6 @@ from django.core.files.storage import FileSystemStorage
 from .models import MedicalRecord, Category, Reminder
 from django.utils import timezone
 import uuid
-from fpdf import FPDF  # For generating PDFs
 from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from django.contrib.auth.forms import AuthenticationForm
